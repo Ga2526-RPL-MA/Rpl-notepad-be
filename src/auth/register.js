d79251a8c1c3b8fef22208d63100b1556da019ca
Andhika Aryaputra <andhika.aryaputra@gmail.com>
2025-10-26 20:19:04 +0700

    test

--- src/auth/register.js
+++ src/auth/register.js
@@ -3,3 +3,5 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+
+const awasadasule = [];
