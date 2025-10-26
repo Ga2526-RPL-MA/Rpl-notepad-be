da7e09ead8dea30006d0053f9da00aa050615996
Andhika Aryaputra <andhika.aryaputra@gmail.com>
2025-10-26 23:14:44 +0700

    feat: register user

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+const registerRouter = require('./auth/register');
+app.use('/register', registerRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
