805b33eb1c7e22bd5d7015f4722266b62482da97
Andhika Aryaputra <andhika.aryaputra@gmail.com>
2025-11-25 22:48:46 +0700

    update: delete files in storage when notes deleted

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -111,6 +111,31 @@ router.delete('/:id', authenticateToken, async (req, res) => {
         await prisma.note.delete({
             where: { id: parseInt(id) }
         });
+
+        const { data, error } = await supabase.storage
+            .from('notes')
+            .list(`notes/${id}/`)
+
+        if (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Error reading folder' });
+        }
+
+        if (!data || data.length === 0) {
+            return res.json({ error: 'No files in storage' });
+        }
+
+        const paths = data.map(files => `notes/${id}/${files.name}`);
+
+        const { deleteError } = await supabase.storage
+            .from('notes')
+            .remove(paths);
+
+        if (deleteError) {
+            console.error(deleteError);
+            return res.status(500).json({ error: 'Error deleting note' });
+        }
+
         res.status(204).end()
     }
     catch (error) {
